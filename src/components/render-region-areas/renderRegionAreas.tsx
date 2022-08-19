b04e56f14eb955b805f3824e96008dfee3fc9a7e
Mikabona96 <76862391+Mikabona96@users.noreply.github.com>
2022-08-19 13:25:42 +0300

    Add unit (#3)
    * regionClicked & selectUnits now in /tools/helpers
    
    * addNewUnitsModal added
    
    * add new unit functionality added
    
    * add new Unit

--- src/components/render-region-areas/renderRegionAreas.tsx
+++ src/components/render-region-areas/renderRegionAreas.tsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import { Region } from '../../models/region'
+import { regionClicked, RegionClickedTypes } from '../../tools/helpers/regionClicked'
 
 type PropTypes = {
     regions: Region[]
-    regionClicked: (region: Region) => void
+    regionClicked: RegionClickedTypes
 }
 
-export const RenderRegionAreas = ({regions, regionClicked}: PropTypes) => {
-    
+export const RenderRegionAreas = ({regions, regionClicked: regionClickedProp}: PropTypes) => {
+    const {dispatch, setSelectedRegion, SelectedRegion, showUnitsMenu} = regionClickedProp;
   return (
     <>
         {
             regions.map((region, i) => {
                 const coords = [region.xposition, region.yposition, 25].join(',');
                 return (
-                    <area key={i} className='region' shape="circle" onClick={() => regionClicked(region)} coords={coords} alt = "" />
+                    <area 
+                    key={i} 
+                    className='region' 
+                    shape="circle" 
+                    onClick={
+                        () => regionClicked({region, dispatch, setSelectedRegion, SelectedRegion, showUnitsMenu})
+                    } 
+                    coords={coords} alt = "" />
                 )
             })
         }
--- src/pages/board/Board.tsx
+++ src/pages/board/Board.tsx
@@ -1,79 +1,121 @@
 import { useState } from "react";
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Form } from "react-bootstrap";
 import { useAppSelector, useAppDispatch } from "../../tools/hooks/hooks";
 import { RenderRegionAreas } from "../../components/render-region-areas/renderRegionAreas";
 import { RenderRegions } from "../../components/render-regions/renderRegions";
 import { Region } from "../../models/region";
-import { Unit } from "../../models/unit";
 import { selectRegions, setRegionUnits } from "../../redux/game/gameSlice";
 import './Board.css';
+import { selectUnit } from "../../tools/helpers/selectUnit";
+import { Side } from "../../models/side";
+import { Faction } from "../../models/faction";
+import { UnitType } from "../../models/unitType";
+import { Unit } from "../../models/unit";
 
 export function Board(props: any) {
     const [ShowUnitsMenu, showUnitsMenu]=useState(false);
     const [SelectedRegion, setSelectedRegion]=useState(null as Region);
+    const [showAddNewUnitsModal, setShowAddNewUnitsModal] = useState(false);
+    const [selectedSideOfUnit, setSelectedSideOfUnit] = useState(Side.FreePeople)
+    const [selectFactionOfUnit, setSelectedFactionOfUnit] = useState(Faction.Elves)
+    const [selectedUnitType, setSelectedUnitType] = useState(UnitType.Regular)
+
     const regions = useAppSelector(selectRegions);
     const dispatch = useAppDispatch();
 
-
-    function regionClicked(region: Region): void {
-        if (!SelectedRegion) {
-            if (!region.units || region.units.length === 0) return;
-
-            region.units.map(x => x.selected = true);
-            showUnitsMenu(true);
-            setSelectedRegion(region);
-        } else {
-            if (region.key === SelectedRegion.key) return;
-
-            let unitsArray = [];
-            unitsArray = unitsArray.concat(SelectedRegion.units.filter(x => x.selected));
-            unitsArray = unitsArray.concat(region.units);
-            dispatch(setRegionUnits({
-                regionKey: region.key,
-                units: unitsArray
-            }));
-            dispatch(setRegionUnits({
-                regionKey: SelectedRegion.key,
-                units: SelectedRegion.units.filter(x => !x.selected)
-            }));
-
-            showUnitsMenu(false);
-            setSelectedRegion(null);
-        }    
-    }
-
-    function selectUnit(unit: Unit) {
-        let region = { ...SelectedRegion };
-        region.units.find(x => x.key === unit.key).selected = !unit.selected;
-        setSelectedRegion(region);
+    const addNewUnit = () => {
+        const newUnit = new Unit(selectedSideOfUnit, selectFactionOfUnit, selectedUnitType)
+        const units = [...SelectedRegion.units, newUnit]
+        dispatch(setRegionUnits({
+            regionKey: SelectedRegion.key,
+            units: units
+        }));
+        const tempRegion = {
+            ...SelectedRegion,
+            units: units
+        }
+        setSelectedRegion(tempRegion)
     }
 
     return (
         <div className='board'>
             <img className='boardMap' src={"images/board.jpg"} useMap="#Map" alt=""/>
 
-            <RenderRegions regions = {regions} regionClicked = {regionClicked} />
+            <RenderRegions regions = {regions} regionClicked = {{showUnitsMenu, setSelectedRegion, dispatch, SelectedRegion}} />
 
             <map name="Map" id="Map">
-                <RenderRegionAreas regions = {regions} regionClicked = {regionClicked}/>
+                <RenderRegionAreas regions = {regions} regionClicked = {{showUnitsMenu, setSelectedRegion, dispatch, SelectedRegion}}/>
             </map>
 
             {ShowUnitsMenu && 
-            <Card className="unitsMenu">
-                <Card.Body>
-                    <Card.Title>Selected Army</Card.Title>
-                    <div className="selectableUnitsBlock">
-                        {SelectedRegion.units.map((unit, i) => {    
-                            return (
-                                <div key={i} className={ "selectableUnit" + (unit.selected ? " selected" : "") } onClick={() => { selectUnit(unit) }}>
-                                    <img className="selectedUnit" style={{height: '50px'}} src={unit.imageUrl} alt=""/>
-                                </div>
-                            ) 
-                        })}
-                    </div>
-                    <Button variant="primary" onClick={() => {showUnitsMenu(false); setSelectedRegion(null);}}>Cancel</Button>
-                </Card.Body>
-            </Card>}            
+            <>
+                <Card className="unitsMenu">
+                    <Card.Body className="SelectedArmyCard">
+                        <Card.Title>Selected Army</Card.Title>
+                        <div className="selectableUnitsBlock">
+                            {SelectedRegion.units.map((unit, i) => {    
+                                return (
+                                    <div key={i} className={ "selectableUnit" + (unit.selected ? " selected" : "") } onClick={() => { selectUnit({unit, setSelectedRegion, SelectedRegion}) }}>
+                                        <img className="selectedUnit" style={{height: '50px'}} src={unit.imageUrl} alt=""/>
+                                    </div>
+                                ) 
+                            })}
+                        </div>
+                        <div className="buttonGroup">
+                            <Button variant="secondary" onClick={() => { setShowAddNewUnitsModal(true) }}>Add</Button>
+                            <Button variant="primary" onClick={() => {showUnitsMenu(false); setSelectedRegion(null); setShowAddNewUnitsModal(false);}}>Cancel</Button>
+                        </div>
+                    </Card.Body>
+                </Card>
+                {showAddNewUnitsModal && <Card className="AddNewUnitsMenu">
+                    <Card.Body className="AddNewUnitsCard">
+                        <Card.Title>Add new Units</Card.Title>
+                        <div className="selectableUnitsBlock">
+                            <span>Side select</span>
+                        <Form.Select size="sm" onChange={(e) => setSelectedSideOfUnit(Side[e.currentTarget.value])}>
+                            <option>FreePeople</option>
+                            <option>SauronForces</option>
+                        </Form.Select>
+                        <br />
+                        <span>Faction select</span>
+                        <Form.Select size="sm" onChange={(e) => setSelectedFactionOfUnit(Faction[e.currentTarget.value])}>
+                            {
+                                selectedSideOfUnit !== Side.SauronForces 
+                                &&
+                                <>
+                                    <option>Elves</option>
+                                    <option>Dwarfs</option>
+                                    <option>Northmen</option>
+                                    <option>Gondor</option>
+                                    <option>Rohan</option>
+                                </>
+                            }
+                            {
+                                selectedSideOfUnit === Side.SauronForces 
+                                &&
+                                <>
+                                    <option>Sauron</option>
+                                    <option>Isengard</option>
+                                    <option>Easterlings</option>
+                                </>
+                            }
+                        </Form.Select>
+                        <br />
+                            <span>Unit type select</span>
+                        <Form.Select size="sm" onChange={(e) => setSelectedUnitType(UnitType[e.currentTarget.value])}>
+                            <option>Regular</option>
+                            <option>Elite</option>
+                            <option>Leader</option>
+                        </Form.Select>
+                        </div>
+                        <div className="buttonGroup">
+                            <Button variant="secondary" onClick={() => addNewUnit()}>Add</Button>
+                            <Button variant="primary" onClick={() => {setShowAddNewUnitsModal(false)}}>Cancel</Button>
+                        </div>
+                    </Card.Body>
+                </Card>}
+            </>
+            }            
         </div>
     );
 }
