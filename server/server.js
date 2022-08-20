27087cd813dce284d887c330f2893218c0733002
alexander2396 <alex.kalashnikov2396@gmail.com>
2022-08-20 21:31:09 +0300

    move public folder

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(`${__dirname}/../client/build`));
+app.use(express.static(path.join(__dirname + "public")));
 
 const PORT = process.env.PORT || 3000
 
