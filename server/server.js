0992a49fc8d0317ff21034cd8853f6a469ec3b7f
alexander2396 <alex.kalashnikov2396@gmail.com>
2022-08-20 21:27:06 +0300

    update port

--- server/server.js
+++ server/server.js
@@ -5,12 +5,14 @@ const app = express();
 
 app.use(express.static(`${__dirname}/../client/build`));
 
+const PORT = process.env.PORT || 3000
+
 const server = http.createServer(app);
 
 server.on('error', (err) => {
     console.error(err);
 });
 
-server.listen(8080, () => {
+server.listen(PORT, () => {
     console.log('server is ready');
 });
