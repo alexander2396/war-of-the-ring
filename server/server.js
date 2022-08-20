c172258d96c10bf424c55235b9142ff1804824f8
alexander2396 <alex.kalashnikov2396@gmail.com>
2022-08-20 21:33:38 +0300

    update server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 const http = require("http");
 const express = require('express');
+const path = require('path')
 
 const app = express();
 
-app.use(express.static(path.join(__dirname + "public")));
+app.use(express.static(path.join(__dirname + "/public")));
 
 const PORT = process.env.PORT || 3000
 
