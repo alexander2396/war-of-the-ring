3289a489f9708f0845aad16d9d19460f42ec8428
alexander2396 <alex.kalashnikov2396@gmail.com>
2022-08-20 22:33:38 +0300

    update server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,15 +2,19 @@ import './App.css';
 import { AppMenu } from './pages/menu/AppMenu';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Board } from './pages/board/Board';
+import { io } from 'socket.io-client';
 
 function App() {
   
-  return (
-    <div className="App">
-      <AppMenu/>
-      <Board/>
-    </div>
-  );
+    const sock = io("http://localhost:3000");
+    sock.on('message', (text) => console.log(text));
+
+    return (
+        <div className="App">
+            <AppMenu/>
+            <Board/>
+        </div>
+    );
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const socketio = require('socket.io');
 
 const app = express();
 
@@ -9,6 +10,16 @@ app.use(express.static(path.join(__dirname + "/public")));
 const PORT = process.env.PORT || 3000
 
 const server = http.createServer(app);
+const io = socketio(server, {
+    cors: {
+      origin: "http://localhost:3001",
+      methods: ["GET", "POST"]
+    }
+})
+
+io.on('connection', (sock) => {
+    sock.emit('message', 'You are connected');
+});
 
 server.on('error', (err) => {
     console.error(err);
